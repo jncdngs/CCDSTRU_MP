4b6935ffe9cfaacd359551559969b8744ae8140f
jncdngs <jonchesterdonguines07@gmail.com>
2025-03-22 17:48:28 +0800

    Implement displayBoard()

--- donguines.c
+++ donguines.c
@@ -2,16 +2,56 @@
 
 #define SIZE 4
 
+void displayBoard()
+{
+    int i, j;
+    
+    for(i = 0; i < SIZE * 2 + 2; i++)
+    {
+        if(i == 0)       // First row
+        {
+            printf("     ");
+            
+            for(j = 0; j < SIZE; j++)
+                printf("%d   ", j + 1);
+
+            printf("\n");
+        }
+        else if(i % 2)  // Odd rows
+        {
+            printf("   ");
+
+            for(j = 0; j < SIZE; j++)
+                printf("+---", j + 1);
+
+            printf("+\n");
+        }
+        else            // Even rows
+        {
+            printf("%d  ", i / 2);
+
+            for(j = 0; j < SIZE; j++)
+                printf("|   ");
+            
+            printf("|\n");
+        }
+    }
+
+    printf("\n");
+}
+
 int main()
 {
-    int turn = 1;
-    int go = 0;
-    int over = 0;
+    // int turn = 1;
+    // int go = 0;
+    // int over = 0;
 
-    int uno[SIZE][SIZE] = {0};
-    int dos[SIZE][SIZE] = {0};
-    int tres[SIZE][SIZE] = {0};
-    int free[SIZE][SIZE] = {0};
+    // int uno[SIZE][SIZE] = {0};
+    // int dos[SIZE][SIZE] = {0};
+    // int tres[SIZE][SIZE] = {0};
+    // int free[SIZE][SIZE] = {0};
+
+    displayBoard();
 
     return 0;
 }
