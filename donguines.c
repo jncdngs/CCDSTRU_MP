1d9e8903cf8925de9f55185f95bbbdc11091faf6
jncdngs <jonchesterdonguines07@gmail.com>
2025-03-22 17:07:15 +0800

    Declare initial variables

--- donguines.c
+++ donguines.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 
+#define SIZE 4
+
 int main()
 {
-    printf("Hello!\n");
-    printf("This is a placeholder file.\n");
+    int turn = 1;
+    int go = 0;
+    int over = 0;
+
+    int uno[SIZE][SIZE] = {0};
+    int dos[SIZE][SIZE] = {0};
+    int tres[SIZE][SIZE] = {0};
+    int free[SIZE][SIZE] = {0};
 
     return 0;
 }
