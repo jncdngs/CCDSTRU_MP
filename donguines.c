8f37c9c18eee97e3c71664adeeb881d5cb6fd38e
jncdngs <jonchesterdonguines07@gmail.com>
2025-03-25 00:11:07 +0800

    Implement isGameOver()

--- donguines.c
+++ donguines.c
@@ -7,16 +7,16 @@ void cls()
     printf("\033[H\033[J\033[3J");
 }
 
-void initBoard(int board[][SIZE])
+void initBoard(char board[][SIZE])
 {
     int i, j;
 
     for(i = 0; i < SIZE; i++)
         for(j = 0; j < SIZE; j++)
-            board[i][j] = 0;
+            board[i][j] = ' ';
 }
 
-void displayBoard(int board[][SIZE])
+void displayBoard(char board[][SIZE])
 {
     int i, j;
     int row = 0;
@@ -48,8 +48,10 @@ void displayBoard(int board[][SIZE])
             printf("%d  ", i / 2);
 
             for(j = 0; j < SIZE; j++)
-                printf("| %d ", board[row][j]);
-            
+            {
+                    printf("| %c ", board[row][j]);
+            }
+
             printf("|\n");
             row++;
         }
@@ -58,21 +60,94 @@ void displayBoard(int board[][SIZE])
     printf("\n");
 }
 
+int isGameOver(char board[][SIZE])
+{
+    int i, j;
+    int over = 0;
+    int full = 1;
+    char player;
+
+    for(i = 0; i < SIZE; i++)
+        for(j = 0; j < SIZE; j++)
+            if(board[i][j] == ' ')
+                full = 0;
+    
+    if(full == 1)
+        over = 2;
+    else
+    {
+        for(player = '1'; player <= '3'; player += 2)
+        {
+            if(((board[0][0] == player || board[0][0] == 'X') && 
+                (board[0][1] == player || board[0][1] == 'X') && 
+                (board[0][2] == player || board[0][2] == 'X') && 
+                (board[0][3] == player || board[0][3] == 'X')) || 
+    
+               ((board[0][0] == player || board[0][0] == 'X') && 
+                (board[1][1] == player || board[1][1] == 'X') && 
+                (board[2][2] == player || board[2][2] == 'X') && 
+                (board[3][3] == player || board[3][3] == 'X')) || 
+    
+               ((board[0][3] == player || board[0][3] == 'X') && 
+                (board[1][2] == player || board[1][2] == 'X') && 
+                (board[2][1] == player || board[2][1] == 'X') && 
+                (board[3][0] == player || board[3][0] == 'X')) || 
+            
+               ((board[3][0] == player || board[3][0] == 'X') && 
+                (board[3][1] == player || board[3][1] == 'X') && 
+                (board[3][2] == player || board[3][2] == 'X') && 
+                (board[3][3] == player || board[3][3] == 'X')))
+                {
+                    over = player - '0';
+                    break;
+                }
+        }
+    }
+    
+    // else if(((board[0][0] == '3' || board[0][0] == 'X') && 
+    //          (board[0][1] == '3' || board[0][1] == 'X') && 
+    //          (board[0][2] == '3' || board[0][2] == 'X') && 
+    //          (board[0][3] == '3' || board[0][3] == 'X')) || 
+
+    //         ((board[0][0] == '3' || board[0][0] == 'X') && 
+    //          (board[1][1] == '3' || board[1][1] == 'X') && 
+    //          (board[2][2] == '3' || board[2][2] == 'X') && 
+    //          (board[3][3] == '3' || board[3][3] == 'X')) || 
+
+    //         ((board[0][3] == '3' || board[0][3] == 'X') && 
+    //          (board[1][2] == '3' || board[1][2] == 'X') && 
+    //          (board[2][1] == '3' || board[2][1] == 'X') && 
+    //          (board[3][0] == '3' || board[3][0] == 'X')) || 
+    
+    //         ((board[3][0] == '3' || board[3][0] == 'X') && 
+    //          (board[3][1] == '3' || board[3][1] == 'X') && 
+    //          (board[3][2] == '3' || board[3][2] == 'X') && 
+    //          (board[3][3] == '3' || board[3][3] == 'X')))
+    //     over = 3;
+
+    return over;
+}
+
 int main()
 {
-    int row = -1;
-    int col = -1;
+    int row;
+    int col;
 
     int turn = 1;
     int go = 0;
     int over = 0;
     
-    int board[SIZE][SIZE];
+    char board[SIZE][SIZE];
 
     initBoard(board);
     
+    printf("\n\nTres turn\n");
+
     while(!over)
     {
+        row = -1;
+        col = -1;
+
         displayBoard(board);
 
         do
@@ -89,22 +164,33 @@ int main()
         }    
         while(col < 1 || col > 4);
 
-        if(turn && go && board[col - 1][row - 1] == 0)
+        if(turn && go && board[row - 1][col - 1] == ' ')
         {
-            board[col - 1][row - 1] = 1;
+            board[row - 1][col - 1] = '1';
             turn = !turn;
             go = !go;
+
+            printf("\n\nTres turn\n");
         }
-        else if(!turn && board[col - 1][row - 1] == 1)
+        else if(!turn)
         {
-            board[col - 1][row - 1] = 0;
+            if(board[row - 1][col - 1] == '1' || board[row - 1][col - 1] == '3')
+                board[row - 1][col - 1] = 'X';
+            
             turn = !turn;
+
+            printf("\n\nUno turn\n");
         }
-        else if(turn && !go && board[col - 1][row - 1] == 0)
+        else if(turn && !go && board[row - 1][col - 1] == ' ')
         {
-            board[col - 1][row - 1] = 3;
+            board[row - 1][col - 1] = '3';
             go = !go;
+            
+            printf("\n\nDos turn\n");
         }
-    }        
+
+        over = isGameOver(board);
+    }
+
     return 0;
 }
